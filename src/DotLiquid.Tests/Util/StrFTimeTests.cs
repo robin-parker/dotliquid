3f18cd28f14458cf4571a2e5d1e1edc1382abe44
Robin Parker <robin.parker@hiscox.com>
2021-02-08 23:01:34 +0000

    #251: Enhanced strftime implementation.

--- src/DotLiquid.Tests/Util/StrFTimeTests.cs
+++ src/DotLiquid.Tests/Util/StrFTimeTests.cs
@@ -15,9 +15,13 @@ namespace DotLiquid.Tests.Util
         [TestCase("%A", ExpectedResult = "Sunday")] // Full weekday name (Monday)
         [TestCase("%b", ExpectedResult = "Jan")] // Abbreviated month name (Jan)
         [TestCase("%B", ExpectedResult = "January")] // Full month name (January)
+        [TestCase("%^B", ExpectedResult = "JANUARY")] // Upcased month name
+		[TestCase("%^_10B", ExpectedResult = "   JANUARY")] // Upcase, then Space-pad
+		[TestCase("%_^10B", ExpectedResult = "   JANUARY")] // Space-pad, then Upcase
         [TestCase("%c", ExpectedResult = "Sun Jan 08 14:32:14 2012")] // Date and time representation (Thu Aug 23 14:55:02 2001)
         [TestCase("%C", ExpectedResult = "20")] //Century (20 in 2009)
         [TestCase("%d", ExpectedResult = "08")] // Day of Month, zero-padded (01..31)
+        [TestCase("%-d", ExpectedResult = "8")] // Remove zero-padding
         [TestCase("%D", ExpectedResult = "01/08/12")] // Short date (%m/%d/%y)
         [TestCase("%e", ExpectedResult = " 8")] // Day of month, space-padded ( 1..31)
         // E - not specified
@@ -36,16 +40,20 @@ namespace DotLiquid.Tests.Util
         [TestCase("%l", ExpectedResult = "2")] //Hour in 12-hour format, blank-padded ( 0..12)
         [TestCase("%L", ExpectedResult = "123")] // Millisecond of the second (000..999). The digits under millisecond are truncated to not produce 1000.
         [TestCase("%m", ExpectedResult = "01")] // Month of the year, zero-padded (01..12)
+        [TestCase("%-m", ExpectedResult = "1")] // Remove zero-padding
+        [TestCase("%_m", ExpectedResult = " 1")] // Replace zero-pad with space-padding
+        [TestCase("%_3m", ExpectedResult = "  1")] // Replace single zero-pad with multi-space-pad
+        [TestCase("%010m", ExpectedResult = "0000000001")] // Replace single zero-pad with multi-zero-pad
         [TestCase("%M", ExpectedResult = "32")] // Minute of the hour (00..59)
         [TestCase("%n", ExpectedResult = "\n")] // new-line character (\n)
         // N // Fractional seconds digits, default is 9 digits (nanosecond)
-        // o
-        // O
-        //FIXME:[TestCase("%p", ExpectedResult = "PM")] // Meridian indicator, uppercase (AM/PM)
-        //FIXME:[TestCase("%P", ExpectedResult = "pm")] // Meridian indicator, lowercase (am/pm)
+        // o - not specified
+        // O - not specified
+        [TestCase("%p", ExpectedResult = "PM")] // Meridian indicator, uppercase (AM/PM)
+        [TestCase("%P", ExpectedResult = "pm")] // Meridian indicator, lowercase (am/pm)
         // q - not specified
         [TestCase("%Q", ExpectedResult = "1326033134123")] // Milliseconds since 1970
-        //FIXME:[TestCase("%r", ExpectedResult = "02:32:14 PM")] // 12-hour clock time (02:55:02 PM)
+        [TestCase("%r", ExpectedResult = "02:32:14 PM")] // 12-hour clock time (02:55:02 PM)
         [TestCase("%R", ExpectedResult = "14:32")] // 24-hour HH:MM time, equivalent to %H:%M (14:55)
         [TestCase("%s", ExpectedResult = "1326033134")] // Seconds since 1970
         [TestCase("%S", ExpectedResult = "14")] //Second (00..60)
@@ -56,11 +64,12 @@ namespace DotLiquid.Tests.Util
         [TestCase("%v", ExpectedResult = " 8-JAN-2012")] // VMS date (%e-%^b-%4Y)
         // V // Week number of the week-based year (01..53)
         [TestCase("%w", ExpectedResult = "0")] // Day of the week, Sunday is 0 (0..6)
-        //[TestCase("%W", ExpectedResult = "01")] // Week number with the first Monday as the first day of week one (00..53)
+        [TestCase("%W", ExpectedResult = "01")] // Week number with the first Monday as the first day of week one (00..53)
         [TestCase("%x", ExpectedResult = "08/01/2012")] // Locale Date representation (08/23/01)
         [TestCase("%X", ExpectedResult = "14:32:14")] // Locale Time representation (14:55:02)
         [TestCase("%y", ExpectedResult = "12")] // Year, last two digits (00-99)
         [TestCase("%Y", ExpectedResult = "2012")] // Year with century (2001)
+        [TestCase("%_2Y", ExpectedResult = "2012")] // Ensure space-pad does not truncate.
         [TestCase("%", ExpectedResult = "%")]
         public string TestFormat(string format)
         {
@@ -72,6 +81,7 @@ namespace DotLiquid.Tests.Util
         }
 
         [Test]
+        // NOTE: "%Z" does not match spec: Ruby DateTime.strftime specification states that '%Z' should return a timezone description, such as 'GMT' or 'CST'
         public void TestTimeZone()
         {
             var now = DateTimeOffset.Now;
@@ -81,27 +91,26 @@ namespace DotLiquid.Tests.Util
 
         [TestCase("%z", ExpectedResult = "Z")] // ISO 8601 offset from UTC in timezone (1 minute=1, 1 hour=100) If timezone cannot be determined, no characters (+100)
         [TestCase("%:z", ExpectedResult = "Z")] // hour and minute offset from UTC with a colon (e.g. +09:00)
-        // NOTE: "%Z" does not match spec: the Ruby Time/strftime spec states that '%Z' should return a timezone code, such as 'GMT' or 'CST'
         //[TestCase("%Z", ExpectedResult = "GMT")] //Abbreviated time zone name (.e.g CST) or similar information. (OS dependent)
         public string TestTimeZoneUTC(string format)
         {
             using (CultureHelper.SetCulture("en-GB"))
             {
                 Assert.That(CultureInfo.CurrentCulture, Is.EqualTo(new CultureInfo("en-GB")));
-                return new DateTime(2012, 1, 8, 14, 32, 14, DateTimeKind.Utc).ToStrFTime(format);
+                return DateTime.UtcNow.ToStrFTime(format);
             }
         }
 
         [TestCase("%z", ExpectedResult = "+0100")] // ISO 8601 offset from UTC in timezone (1 minute=1, 1 hour=100) If timezone cannot be determined, no characters (+100)
         [TestCase("%:z", ExpectedResult = "+01:00")] // hour and minute offset from UTC with a colon (e.g. +09:00)
-        // NOTE: "%Z" does not match spec: the Ruby Time/strftime spec states that '%Z' should return a timezone code, such as 'GMT' or 'CST'
         //[TestCase("%Z", ExpectedResult = "BST")] //Abbreviated time zone name (.e.g CST) or similar information. (OS dependent)
         public string TestTimeZoneLocal(string format)
         {
             using (CultureHelper.SetCulture("en-GB"))
             {
                 Assert.That(CultureInfo.CurrentCulture, Is.EqualTo(new CultureInfo("en-GB")));
-                return new DateTime(2012, 6, 10, 14, 32, 14, DateTimeKind.Local).ToStrFTime(format);
+                // Jun-10 is during UK British Summer Time (BST / +01:00)
+                return DateTime.Parse("2012-06-10T14:32:14+01:00").ToStrFTime(format);
             }
         }
     }
--- src/DotLiquid/Util/StrFTime.cs
+++ src/DotLiquid/Util/StrFTime.cs
@@ -1,14 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace DotLiquid.Util
 {
     public static class StrFTime
     {
         public delegate string DateTimeDelegate(DateTime dateTime);
-
-        private static readonly List<String> FORMAT_FLAGS = new List<String>() {":"};
 
         private static readonly Dictionary<string, DateTimeDelegate> Formats = new Dictionary<string, DateTimeDelegate>
         {
@@ -55,15 +55,16 @@ namespace DotLiquid.Util
             { "u", (dateTime) => ((int)(dateTime.DayOfWeek) == 0 ? ((int)(dateTime).DayOfWeek) + 7 : ((int)(dateTime).DayOfWeek)).ToString() },
             { "U", (dateTime) => CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(dateTime, CultureInfo.CurrentCulture.DateTimeFormat.CalendarWeekRule, DayOfWeek.Sunday).ToString().PadLeft(2, '0') },
             { "v", (dateTime) => dateTime.ToString("%d-MMM-yyyy", CultureInfo.CurrentCulture).ToUpper().PadLeft(11, ' ') },
-            // V - not implemented 
+            // V - not implemented
             { "w", (dateTime) => ((int) dateTime.DayOfWeek).ToString() },
-            { "W", (dateTime) => CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(dateTime, CultureInfo.CurrentCulture.DateTimeFormat.CalendarWeekRule, DayOfWeek.Monday).ToString().PadLeft(2, '0') },
+            { "W", (dateTime) => CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(dateTime, CalendarWeekRule.FirstFullWeek, DayOfWeek.Monday).ToString().PadLeft(2, '0') },
             { "x", (dateTime) => dateTime.ToString("d", CultureInfo.CurrentCulture) },
             { "X", (dateTime) => dateTime.ToString("T", CultureInfo.CurrentCulture) },
             { "y", (dateTime) => dateTime.ToString("yy", CultureInfo.CurrentCulture) },
             { "Y", (dateTime) => dateTime.ToString("yyyy", CultureInfo.CurrentCulture) },
-            { "z", (dateTime) => dateTime.ToString("%K").Replace(":", string.Empty) },
-            { ":z", (dateTime) => dateTime.ToString("%K") },
+            { "z", (dateTime) => dateTime.ToString("%K", CultureInfo.CurrentCulture).Replace(":", string.Empty) },
+            { ":z", (dateTime) => dateTime.ToString("%K", CultureInfo.CurrentCulture) },
+            // ::z - not implemented
             { "Z", (dateTime) => dateTime.ToString("zzz", CultureInfo.CurrentCulture) },
             { "%", (dateTime) => "%" } // A % sign
         };
@@ -78,29 +79,62 @@ namespace DotLiquid.Util
         /// <returns>a string version of dateTime matching pattern.</returns>
         public static string ToStrFTime(this DateTime dateTime, string pattern)
         {
-            string output = "";
+            return Regex.Replace(pattern, @"%(?<flag>[-_0^:#])*(?<width>[1-9][0-9]*)?(?<directive>[a-zA-Z%\+])",
+                x => StrFTimeMatchEvaluator(
+                    x.Groups[0].Value,
+                    x.Groups["flag"].Captures.Cast<Capture>().Select(y => y.Value).ToList(),
+                    x.Groups["width"].Captures.Cast<Capture>().Select(y => (int?) Convert.ToInt32(y.Value)).FirstOrDefault(),
+                    x.Groups["directive"].Captures.Cast<Capture>().Select(y => y.Value).FirstOrDefault(),
+                    dateTime
+                    ));
+        }
 
-            int n = 0;
+        private static String StrFTimeMatchEvaluator(String orig, IEnumerable<String> flags, int? width, String directive, DateTime datetime)
+        {
+            directive = PreProcessDirective(directive, flags);
+            if (!Formats.ContainsKey(directive)) return orig;
 
-            while (n < pattern.Length)
+            var result = Formats[directive].Invoke(datetime);
+
+            return flags.ToList().Aggregate(result, (current, flag) => ApplyFlag(flag, (width ?? 2), current));
+        }
+
+        // If the z flag is prefixed with one colons, modify the directive so an alternate Formats Dictionary entry is invoked.
+        private static String PreProcessDirective(String directive, IEnumerable<String> flags)
+        {
+            String result = directive;
+            String flagString = String.Join("", flags);
+            if ("z".Equals(directive) && ":".Equals(flagString))
             {
-                string s = pattern.Substring(n, 1);
-
-                if (n + 1 >= pattern.Length || s != "%")
-                    output += s;
-                else
-                {
-                    string directive = pattern.Substring(++n, 1);
-                    if (FORMAT_FLAGS.Contains(directive) && pattern.Length > n)
-                    {
-                        directive += pattern.Substring(++n, 1); //Add the directove 
-                    }
-                    output += Formats.ContainsKey(directive) ? Formats[directive].Invoke(dateTime) : "%" + directive;
-                }
-                n++;
+                result = flagString + directive;
             }
+            return result;
+        }
 
-            return output;
+        private static String ApplyFlag(String flag, int padwidth, String str)
+        {
+            var result = str;
+            switch (flag)
+            {
+                case "-": //don't pad a numerical output
+                    result = str.TrimStart('0');
+                    break;
+                case "_": //use spaces for padding
+                    result = str.TrimStart('0').PadLeft(padwidth, ' ');
+                    break;
+                case "0": //use zeros for padding
+                    result = str.TrimStart('0').PadLeft(padwidth, '0');
+                    break;
+                case "^": //upcase the result string
+                    result = str.ToUpper();
+                    break;
+                case ":": // handled by PreProcessDirective
+                    break;
+                case "#": // not implemented
+                    break;
+                // default: do nothing.
+            }
+            return result;
         }
     }
 }
